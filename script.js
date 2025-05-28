3fa9a2c29a3d5ea09dc8aadb1d442fe08e815467
sharmilysss7 <sharmilysss7@gmail.com>
2025-05-28 15:46:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add or remove 'scrolled' class on scroll
+window.addEventListener("scroll", () => {
+  const nav = document.getElementById("navbar");
+  nav.classList.toggle("scrolled", window.scrollY > 50);
+});
